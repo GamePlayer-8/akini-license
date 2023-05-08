115f9598b1a388e0682e316b9257edf7eccfbbb7
GamePlayer-8 <gameplayer2019pl@tutamail.com>
2023-05-08 23:12:17 +0200

    Fix CSS.

--- init.sh
+++ init.sh
@@ -10,7 +10,9 @@ echo '<html lang="en-US">' >> index.html
 cat docs/head.html >> index.html
 
 echo '<body>' >> index.html
+echo '<div>' >> index.html
 markdown README.md >> index.html
+echo '</div>' >> index.html
 echo '</body>' >> index.html
 echo '</html>' >> index.html
 
